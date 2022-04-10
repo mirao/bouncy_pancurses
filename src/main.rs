84becbabcd0a3ad4ad9af31493bbfa7fc6474eaa
Jaromir Obr <jaromir.obr@gmail.com>
2022-04-10 12:39:03 +0200

    Refactored creating of game, constants and types of frame/ball areas/coordinates

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+/** Difference between window and frame width/height */
+const WINDOW_FRAME_DIFF: i32 = 4;
+/** Wait between ball moves, in msec */
+const SLEEP_DURATION: i32 = 33;
+
 enum VertDir {
     Up,
     Down,
@@ -9,15 +14,15 @@ enum HorizDir {
 }
 
 struct Ball {
-    x: u32,
-    y: u32,
+    x: i32,
+    y: i32,
     vert_dir: VertDir,
     horiz_dir: HorizDir,
 }
 
 struct Frame {
-    width: u32,
-    height: u32,
+    width: i32,
+    height: i32,
 }
 
 struct Game {
@@ -80,23 +85,7 @@ impl Default for Ball {
 fn main() {
     let window = pancurses::initscr();
 
-    let (max_y, max_x) = window.get_max_yx();
-    let width = max_x as u32 - 4;
-    let height = max_y as u32 - 4;
-    let frame = Frame { width, height };
-
-    // Don't allow small frame
-    if frame.width < 10 || frame.height < 10 {
-        pancurses::endwin();
-        panic!(
-            "Width ({width}) or height ({height}) are too small",
-            height = frame.height,
-            width = frame.width,
-        );
-    }
-
-    let mut game = Game::new(frame);
-    const SLEEP_DURATION: i32 = 33;
+    let mut game = create_game(&window);
     // Disable visibility of block cursor during ball rendering
     pancurses::curs_set(0);
     // Disable showing of input keys
@@ -106,11 +95,11 @@ fn main() {
 
     loop {
         // Hide ball in old position
-        window.mvaddch(game.ball.y as i32, game.ball.x as i32, ' ');
+        window.mvaddch(game.ball.y, game.ball.x, ' ');
         // Move ball
         game.step();
         // Show ball in new position
-        window.mvaddch(game.ball.y as i32, game.ball.x as i32, 'o');
+        window.mvaddch(game.ball.y, game.ball.x, 'o');
         window.refresh(); // Update the screen
 
         // Wait between moves (it doesn't block input key reading)
@@ -129,14 +118,10 @@ fn main() {
             // Terminal size was changed
             Some(pancurses::Input::KeyResize) => {
                 // Change window size
-                let (lines, cols) = window.get_max_yx();
                 pancurses::resize_term(0, 0);
-                // Reset game's frame and ball position
-                game = Game::new(Frame {
-                    height: lines as u32 - 4,
-                    width: cols as u32 - 4,
-                });
-                // Clear existing content
+                // Reset game to initial state
+                game = create_game(&window);
+                // Clear existing frame content
                 window.clear();
                 render_initial_game(&window, &game);
             }
@@ -145,11 +130,28 @@ fn main() {
     }
 }
 
+fn create_game(window: &pancurses::Window) -> Game {
+    let (max_y, max_x) = window.get_max_yx();
+    let width = max_x - WINDOW_FRAME_DIFF;
+    let height = max_y - WINDOW_FRAME_DIFF;
+    let frame = Frame { width, height };
+    // Don't allow small frame
+    if frame.width < 10 || frame.height < 10 {
+        pancurses::endwin();
+        panic!(
+            "Width ({width}) or height ({height}) are too small",
+            height = frame.height,
+            width = frame.width,
+        );
+    }
+    Game::new(frame)
+}
+
 fn render_initial_game(window: &pancurses::Window, game: &Game) {
     // Render game area
     window.border('|', '|', '-', '-', '+', '+', '+', '+');
     // Render ball in initial position
-    window.mvaddch(game.ball.y as i32, game.ball.x as i32, 'o');
+    window.mvaddch(game.ball.y, game.ball.x, 'o');
     // Show updates on screen
     window.refresh();
 }
